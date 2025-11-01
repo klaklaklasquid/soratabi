0861eec6f9739286b153e9f8ebb5ff812098a869
klaklaklasquid <jordyvanbelle@hotmail.com>
2025-11-01 16:29:36 +0100

    Update Footer.tsx
    created the footer, nothing special...

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="flex flex-col items-center gap-2">
+      <h2 className="text-2xl">Reach for the stars</h2>
+      <h3 className="text-xl">Soratabi</h3>
+      <p className="text-sm">&copy; soratabi 2025 Jordy Van Belle</p>
+    </footer>
+  );
 }
 
 export default Footer;
