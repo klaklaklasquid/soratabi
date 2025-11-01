78d7b51873ee80af8ff3d89ea375a6d525be054f
klaklaklasquid <jordyvanbelle@hotmail.com>
2025-11-02 00:21:00 +0100

    small changes for responsive
    did some responsive styling needs more but done for today

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,17 +5,17 @@ function Home() {
   return (
     <div>
       <h2 className="text-2xl">SORATABI</h2>
-      <div className="mt-12 text-4xl">
+      <div className="mt-12 text-4xl sm:text-5xl md:text-6xl">
         <h1>DISCOVER A NEW</h1>
         <h1>WORLD OF</h1>
         <h1>TRAVEL</h1>
+      </div>
 
-        <div className="w-50 h-50 absolute right-5 md:w-60 md:h-60 md:right-10">
-          <Canvas camera={{ fov: 60, position: [0, 0, 2] }}>
-            <ambientLight intensity={1.5} />
-            <Globe />
-          </Canvas>
-        </div>
+      <div className="w-50 h-50 absolute right-5 sm:w-80 sm:h-80 sm:right-20 md:w-100 md:h-100 md:right-30 lg:right-60">
+        <Canvas camera={{ fov: 60, position: [0, 0, 2] }}>
+          <ambientLight intensity={1.5} />
+          <Globe />
+        </Canvas>
       </div>
     </div>
   );
--- src/UI/AppLayout.tsx
+++ src/UI/AppLayout.tsx
@@ -11,14 +11,14 @@ function AppLayout() {
   return (
     <>
       <div
-        className="h-screen w-screen fixed inset-0 bg-cover bg-right -z-20"
-        style={{ backgroundImage: "url('/geminiBg.png')" }}></div>
+        className="h-screen w-screen fixed inset-0 bg-cover bg-top-right -z-20"
+        style={{ backgroundImage: "url('/geminiBgv2.png')" }}></div>
       <div className="flex flex-col z-0">
         <App>
           <NavBar />
 
           {/* Main */}
-          <main className="grow h-svh pl-5 pt-5">
+          <main className={`grow h-svh pl-5 ${!isOpen && "pt-5"}`}>
             {!isOpen ? <Outlet /> : <NavigationLinks />}
           </main>
 
