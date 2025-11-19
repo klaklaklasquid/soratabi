9007fc3e1b5fedead3a392df75836914a43b8a82
klaklaklasquid <jordyvanbelle@hotmail.com>
2025-11-19 21:30:02 +0100

    restore aboutText

---
+++ src/3DComponents/AboutText.tsx
@@ -0,0 +1,59 @@
+import { Canvas } from "@react-three/fiber";
+import Globe from "./Globe";
+import PlaneForText from "./PlaneForText";
+import Paint from "./Paint";
+import Star from "./Star";
+import Rainbow from "./Rainbow";
+
+function AboutText(props: AboutTextProps) {
+  const side = props.number % 2 === 0;
+
+  return (
+    <div
+      className={`bg-primary-blue relative flex h-[300px] w-[300px] flex-col gap-4 rounded-2xl p-5 ${side ? "message-box-right self-start" : "message-box-left self-end"} `}
+    >
+      <h3 className="text-xl">{props.title}</h3>
+      <p>{props.text}</p>
+
+      <div
+        className={`absolute -top-16 h-24 w-24 lg:top-1/2 lg:h-48 lg:w-48 lg:-translate-y-1/2 ${side ? "-left-1/10 lg:left-5/4" : "-right-1/10 lg:right-5/4"}`}
+      >
+        <Canvas camera={{ fov: 60, position: [0, 0, 2] }}>
+          <ambientLight intensity={2} />
+          <ModelRenderer model={props.model} />
+        </Canvas>
+      </div>
+    </div>
+  );
+}
+
+interface AboutTextProps {
+  title: string;
+  text: string;
+  number: number;
+  model: string;
+}
+
+function ModelRenderer({ model }: { model: string }) {
+  switch (model) {
+    case "globe":
+      return <Globe />;
+
+    case "paint":
+      return <Paint />;
+
+    case "star":
+      return <Star />;
+
+    case "plane":
+      return <PlaneForText />;
+
+    case "rainbow":
+      return <Rainbow />;
+
+    default:
+      return null;
+  }
+}
+
+export default AboutText;
